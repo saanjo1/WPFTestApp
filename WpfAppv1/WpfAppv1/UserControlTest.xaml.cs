d80e4171caf7d8b1623970c1a2f492e8d873de4b
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-09-13 17:54:56 +0200

    Starter Solution - Created first UserControl with required buttons

---
+++ WpfAppv1/WpfAppv1/UserControlTest.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfAppv1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class UserControlTest : UserControl
+    {
+        public UserControlTest()
+        {
+            InitializeComponent();
+        }
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ImportCat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExportCat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+
