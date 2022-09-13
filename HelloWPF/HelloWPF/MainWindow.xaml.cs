7f83f6009c927b1062d6bb41062c5851c3d8a2cb
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-09-13 13:58:40 +0200

    Testni zadatak CurrentCulture

---
+++ HelloWPF/HelloWPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = null;
+            try
+            {
+                s.Trim();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("A handled exception just occurred: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            s.Trim();
+        }
+
+        private void CultureInfoSwitchButton_Click(object sender, RoutedEventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo((sender as Button).Tag.ToString());
+            lblNumber.Content = (123456789.42d).ToString("N2");
+            lblDate.Content = DateTime.Now.ToString();
+        }
+    }
+}
