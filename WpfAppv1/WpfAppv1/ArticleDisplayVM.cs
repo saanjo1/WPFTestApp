0374c319be4683534399fc27559b1dc843f2bf3d
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-09-14 11:22:58 +0200

    Article VM Created, Display all the records from Excel (only necessary columns). Version 1

---
+++ WpfAppv1/WpfAppv1/ArticleDisplayVM.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppv1
+{
+    public class ArticleDisplayVM
+    {
+        public string ID { get; set; }
+        public string ItemName { get; set; }
+        public string Prijevodi { get; set; }
+        public string ColorDescription { get; set; }
+        public string ItemSize { get; set; }
+        public string Gender { get; set; }
+        public string BarCode { get; set; }
+        public string So_Price { get; set; }
+    }
+
+    public class ExcelDataService
+    {
+        OleDbConnection conn;
+        OleDbCommand Command;
+
+        public ExcelDataService()
+        {
+            string excelfile = @"C:\Users\asus\OneDrive\Desktop\DokumentacijaPraksa\ExcelFile.xls";
+            string con =
+                  @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + excelfile + ";" +
+                  @"Extended Properties='Excel 8.0;HDR=Yes;';Persist Security Info=True";
+            conn = new OleDbConnection(con);
+        }
+
+
+        public async Task<ObservableCollection<ArticleDisplayVM>> ReadFromExcel()
+        {
+            ObservableCollection<ArticleDisplayVM> Articles = new ObservableCollection<ArticleDisplayVM>();
+
+            await conn.OpenAsync();
+            Command = new OleDbCommand();
+            Command.Connection = conn;
+            Command.CommandText = "select * from [DETAILS$]";
+
+            var Reader = await Command.ExecuteReaderAsync();
+
+            while (Reader.Read())
+            {
+                Articles.Add(new ArticleDisplayVM()
+                {
+                    ID = Reader["ITEM"].ToString(),
+                    BarCode = Reader["BARCODE"].ToString(),
+                    ItemName = Reader["ITEM"].ToString() + Reader["BARCODE"].ToString(),
+                    ColorDescription = Reader["COLOR_DESCRIPTION"].ToString(),
+                    Gender = Reader["GENDER"].ToString(),
+                    So_Price = Reader["SO_PRICE"].ToString(),
+                    Prijevodi = Reader["prijevodi HRVATSKI"].ToString(),
+                    ItemSize = Reader["ITEM_SIZE"].ToString()
+                });
+            }
+
+            Reader.Close();
+            conn.Close();
+            return Articles;
+        }
+    }
+}
--- WpfAppv1/WpfAppv1/UserControlTest.xaml.cs
+++ WpfAppv1/WpfAppv1/UserControlTest.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,13 +22,25 @@ namespace WpfAppv1
     /// </summary>
     public partial class UserControlTest : UserControl
     {
+        ExcelDataService _objExcelSer;
+        ArticleDisplayVM _article = new ArticleDisplayVM();
+
         public UserControlTest()
         {
             InitializeComponent();
         }
-        private void Import_Click(object sender, RoutedEventArgs e)
-        {
 
+            private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            _objExcelSer = new ExcelDataService();
+            try
+            {
+                dataGridStudent.ItemsSource = _objExcelSer.ReadFromExcel().Result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void Export_Click(object sender, RoutedEventArgs e)
@@ -40,9 +53,23 @@ namespace WpfAppv1
 
         }
 
-        private void ExportCat_Click(object sender, RoutedEventArgs e)
+        private void ExportCat_Click(object sender, RoutedEventArgs en)
+        {
+            string excel_path = @"C:\Users\asus\OneDrive\Desktop\DokumentacijaPraksa\ExcelFile.xls";
+            string con =
+               @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + excel_path + ";" +
+               @"Extended Properties='Excel 8.0;HDR=Yes;';Persist Security Info=True";
+            using (OleDbConnection connection = new OleDbConnection(con))
+            {
+
+            }
+        }
+
+        public static bool ImportArticlesFromExcel(string excel_path = @"C:\Users\asus\OneDrive\Desktop\DokumentacijaPraksa\ExcelFile.xls", string tab_name = "Sheet1$", int barcode_index = 1, int name_index = 1, int price_ind = 1)
         {
 
+            
+            return true;
         }
     }
 }
