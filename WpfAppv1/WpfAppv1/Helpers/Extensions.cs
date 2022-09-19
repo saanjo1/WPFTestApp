65d9ee2184e210e766d6e734598b89382e2052da
Sanjin Gološ <sanjin.golos@edu.fit.ba>
2022-09-20 00:37:12 +0200

    Extensions update

--- WpfAppv1/WpfAppv1/Helpers/Extensions.cs
+++ WpfAppv1/WpfAppv1/Helpers/Extensions.cs
@@ -11,9 +11,25 @@ namespace WpfAppv1.Helpers
     {
        private static possectorContext appContext = new possectorContext();
 
+
+        public static void ManageCategories()
+        {
+            if (!appContext.Categories.Any())
+            {
+                appContext.Categories.Add(new Category
+                {
+                    Id = new Guid("59f06472-79f7-4fda-a2d4-f24f1eae6e1b"),
+                    Name = "default",
+                    Deleted = false,
+                    Order = 1
+                });
+                appContext.SaveChanges();
+            }
+        }
         public static Guid ManageSubcategory(string gender)
         {
             var x = appContext.SubCategories.Where(x => x.Name == gender).FirstOrDefault();
+            ManageCategories();
             if (x == null)
             {
                 SubCategory subCategory = new SubCategory()
@@ -36,10 +52,13 @@ namespace WpfAppv1.Helpers
 
         public static decimal GetDecimal(string value)
         {
-            if (value == "" && value == null)
-                return decimal.Parse("0");
+            decimal decimalValue;
+            if (value == "" || value == null)
+                decimalValue = decimal.Parse("0");
+            else
+                decimalValue = decimal.Parse(value);
 
-            return decimal.Parse(value);
+            return decimalValue;
         }
     }
 }
